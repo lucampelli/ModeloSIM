895b6b15bde2b4ac8161983ee733e49daca04889
lucampelli <lucampelli@hotmail.com>
2016-09-21 16:21:26 -0300

    Utilities!
    woo

---
+++ SimuladorSmail/src/simuladorsmail/Sistema.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simuladorsmail;
+
+/**
+ *
+ * @author Luca
+ */
+public class Sistema {
+    public Sistema(){
+        System.out.println(Utilities.CONST(15));
+        System.out.println(Utilities.NORM(0, 0));
+        System.out.println(Utilities.TRIA(0.1f, 0.2f, 0.34f));
+        System.out.println(Utilities.UNIF(0.2f, 0.55f));
+        Utilities.proxTempo(13);
+        Utilities.proxTempo(13);
+        Utilities.setDelayAmount(2);
+        Utilities.setUnit('m');
+        System.out.println(Utilities.getTempo());
+        System.out.println(Utilities.getTempoAnt());
+        System.out.println(Utilities.getUnit());
+        
+    }
+}
--- SimuladorSmail/src/simuladorsmail/Utilities.java
+++ SimuladorSmail/src/simuladorsmail/Utilities.java
@@ -15,6 +15,47 @@ Guarda o tempo atual e possui os geradores de números aleatórios.
 */
 //Tempo Atual: int tempoAtual;
 
-public class Utilities {
+public abstract class Utilities {
+    private static int tempoAnterior = 0;
+    private static int tempoAtual = 0;
+    private static char unit = 'h'; //horas, minutos, segundos.
+    private static float delayAmount = 0;
     
+    public static int getTempo(){
+        return tempoAtual;
+    }
+    public static void proxTempo(){
+        tempoAnterior = tempoAtual;
+        tempoAtual++;
+    }
+    public static void proxTempo(int skip){
+        tempoAnterior = tempoAtual;
+        tempoAtual += skip;
+    }
+    public static int getTempoAnt(){
+        return tempoAnterior;
+    }
+    public static void setUnit(char newUnit){
+        unit = newUnit;
+    }
+    public static char getUnit(){
+        return unit;
+    }
+    public static void setDelayAmount(float delay){
+        delayAmount = delay;
+    }
+    public static float NORM(float M, float DP){
+        return 0f;
+    }
+    public static float TRIA(float S, float MID, float E){
+        return 0f;
+    }
+    public static float UNIF(float U, float S){
+        return 0f;
+    }
+    public static float CONST(float cons){
+        return cons;
+    }
+    
+           
 }
