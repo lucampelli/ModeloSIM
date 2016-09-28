46686867984084f9a99cdb46571e26d70a99ebe9
RodrigoBonacossa <rodrigo_bonacos@hotmail.com>
2016-09-28 20:28:32 -0300

    ServiceModuleV1.0
    scdhgiu

--- SimuladorSmail/src/simuladorsmail/Evento.java
+++ SimuladorSmail/src/simuladorsmail/Evento.java
@@ -10,35 +10,37 @@ package simuladorsmail;
  * @author Luca
  */
 public class Evento {
-    
-    public static enum tipoDeEvento {CRIACAOL,CRIACAOR,SELECAO,SERVICO,SAIDA,FINAL} 
+
+    public static enum tipoDeEvento {
+
+        CRIACAOL, CRIACAOR, SELECAO, SERVICO, SAIDA, FINAL
+    }
     private float tempo = 0;
     private tipoDeEvento tipo;
     private Module responsa;
     private Entity entidade;
-    
-    
-    public Evento (tipoDeEvento Tipo, float tempo, Module responsavel, Entity e){
+
+    public Evento(tipoDeEvento Tipo, float tempo, Module responsavel, Entity e) {
         this.tipo = Tipo;
         this.tempo = tempo;
         this.responsa = responsavel;
         this.entidade = e;
     }
-    
-    public Module getResponsavel(){
+
+    public Module getResponsavel() {
         return responsa;
     }
-    
-    public tipoDeEvento getTipo(){
+
+    public tipoDeEvento getTipo() {
         return tipo;
     }
-    
-    public float getTempo(){
+
+    public float getTempo() {
         return tempo;
     }
-    
-    public Entity getEntity(){
+
+    public Entity getEntity() {
         return entidade;
     }
-    
+
 }
--- SimuladorSmail/src/simuladorsmail/Sistema.java
+++ SimuladorSmail/src/simuladorsmail/Sistema.java
@@ -6,50 +6,48 @@
 package simuladorsmail;
 
 //For testing purposes, or maybe not
-
 import static java.lang.Thread.sleep;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-
 public class Sistema {
-    
+
     private ArrayList<Evento> filaDeEventos;
     private StartEntity se;
     private ReceptModule rm;
     private ServiceModule sml;
     private ServiceModule smr;
     private EndEntity ee;
-    
-    public Sistema(){
+
+    public Sistema() {
         filaDeEventos = new ArrayList<Evento>();
         se = new StartEntity(this);
         rm = new ReceptModule(this);
         sml = new ServiceModule(this);
         smr = new ServiceModule(this);
         ee = new EndEntity();
-        
+
         createEvent(Evento.tipoDeEvento.CRIACAOL, Utilities.EXPO(0.5f), null, null);
         createEvent(Evento.tipoDeEvento.CRIACAOR, Utilities.EXPO(0.6f), null, null);
-        
-        while (!filaDeEventos.isEmpty()){
+
+        while (!filaDeEventos.isEmpty()) {
             Evento atual = getNextEvent();
             Run(atual);
         }
     }
-    
-    public void createEvent(Evento.tipoDeEvento tipo, float tempo, Module r, Entity e){
-        filaDeEventos.add(new Evento(tipo,tempo,r,e));
+
+    public void createEvent(Evento.tipoDeEvento tipo, float tempo, Module r, Entity e) {
+        filaDeEventos.add(new Evento(tipo, tempo, r, e));
     }
-    
-    public Evento getNextEvent(){
-        
+
+    public Evento getNextEvent() {
+
         Evento chosen = filaDeEventos.get(0);
         float tempo = chosen.getTempo();
-        
-        for(Evento e : filaDeEventos){
-            if(e.getTempo() < tempo){
+
+        for (Evento e : filaDeEventos) {
+            if (e.getTempo() < tempo) {
                 chosen = e;
                 tempo = e.getTempo();
             }
@@ -58,37 +56,36 @@ public class Sistema {
         filaDeEventos.remove(chosen);
         return chosen;
     }
-    
-    public void Run(Evento e){
-        if(e.getTipo() == Evento.tipoDeEvento.CRIACAOL){
+
+    public void Run(Evento e) {
+        if (e.getTipo() == Evento.tipoDeEvento.CRIACAOL) {
             se.createEvent(true);
         }
-        if(e.getTipo() == Evento.tipoDeEvento.CRIACAOR){
+        if (e.getTipo() == Evento.tipoDeEvento.CRIACAOR) {
             se.createEvent(false);
         }
-        if(e.getTipo() == Evento.tipoDeEvento.SELECAO){
+        if (e.getTipo() == Evento.tipoDeEvento.SELECAO) {
             rm.createEvent(e.getEntity());
         }
-        if(e.getTipo() == Evento.tipoDeEvento.SERVICO){
-            
+        if (e.getTipo() == Evento.tipoDeEvento.SERVICO) {
+
         }
-        if(e.getTipo() == Evento.tipoDeEvento.SAIDA){
-            
+        if (e.getTipo() == Evento.tipoDeEvento.SAIDA) {
+
         }
-        if(e.getTipo() == Evento.tipoDeEvento.FINAL){
-            
+        if (e.getTipo() == Evento.tipoDeEvento.FINAL) {
+
         }
     }
-    
-    
-    public void printFilaDeEventos(){
-        for(Evento e : filaDeEventos){
+
+    public void printFilaDeEventos() {
+        for (Evento e : filaDeEventos) {
             System.out.println();
         }
     }
-    
-    public ServiceModule getServiceModule(boolean local){
-        if(local){
+
+    public ServiceModule getServiceModule(boolean local) {
+        if (local) {
             return sml;
         } else {
             return smr;
